da46ac38eb1bdd1d92b7c83fec611fb71f4f6291
JiliJiang <jiangjili@gmail.com>
2023-08-18 21:55:08 -0700

    starter code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static int myNumber = 9;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!" + myNumber);
+    }
+}
